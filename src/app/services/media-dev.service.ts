ef6ff88a9c2b71665f7abbe8a5aee362c062ffb8
Marcot132 <marco.corona547@gmail.com>
2023-10-16 22:58:47 -0600

    create branch stddev-#8

---
+++ src/app/services/media-dev.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs/';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MediaDevService {
+
+  constructor(
+    private readonly http: HttpClient
+  ) { }
+
+  apiURL = 'https://marco-service-marcot132.cloud.okteto.net/dev-size'
+
+
+  //http opcions
+
+  httpOpcions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  getData(): Observable<any> {
+    return this.http.get<any>(this.apiURL, this.httpOpcions).pipe(
+      retry(1),
+      catchError(this.handleError)
+    )
+  }
+
+  handleError(error: any) {
+    let errorMessage = ''
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Error code ${error.status} Mensage: ${error.message}`
+    }
+    window.alert(errorMessage)
+    return throwError(errorMessage)
+  }
+}
